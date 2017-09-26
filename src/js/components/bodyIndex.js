76abff63084ceb4f315bb873a03e7b797273c483
yanlele <331393627@qq.com>
2017-09-26 16:24:20 +0800

    参数的绑定！

--- src/js/components/bodyIndex.js
+++ src/js/components/bodyIndex.js
@@ -2,13 +2,18 @@ import React, {Component} from 'react'
 
 export default class BodyIndex extends Component {
     render() {
-        var userName='parry';
+        var userName='parry123123';
         var boolInput=false;
+
+        var html="yanle   &nbsp;&nbsp;&nbsp;&nbsp;   testProject";
         return (
             <div>
                 <h2>页面的主体内容</h2>
                 <p>{userName==''?'用户没有登录':userName}</p>
-                <p><input type="button" value="默认按钮" disabled={boolInput}/></p>
+                <p><input type="button" value={userName} disabled={boolInput}/></p>
+                {/*注释*/}
+                <p>{html}</p>
+                <p dangerouslySetInnerHTML={{__html:html}}></p>
             </div>
         )
     }
