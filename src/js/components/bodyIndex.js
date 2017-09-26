db9412f1fdba80438c4523994e807eaa622bdfc3
yanlele <331393627@qq.com>
2017-09-26 17:22:39 +0800

    事件绑定的语法：onClick={this.changeUserInfo.bind(this)}

--- src/js/components/bodyIndex.js
+++ src/js/components/bodyIndex.js
@@ -3,23 +3,33 @@ import React, {Component} from 'react'
 export default class BodyIndex extends Component {
     constructor() {
         super();//调用父类的所有初始化方法
-        this.state = {username: 'Parry'};
+        this.state = {
+            username: 'Parry',
+            age:20
+        };
+    }
+
+
+    changeUserInfo() {
+        this.setState({
+            age:18
+        })
     }
 
     render() {
-        setTimeout(() => {
-            //更改state的时候
-            this.setState({
-                username: 'Tom'
-            })
-        }, 4000);
+        /*        setTimeout(() => {
+         //更改state的时候
+         this.setState({
+         username: 'Tom'
+         })
+         }, 4000);*/
 
         var html = "yanle   &nbsp;&nbsp;&nbsp;&nbsp;   testProject";
         return (
             <div>
                 <h2>页面的主体内容</h2>
-                <p>{this.state.username}</p>
-                <p>{this.props.userId}</p>
+                <p>{this.state.username} {this.props.userId} ,age:{this.state.age}</p>
+                <input type="button" value='提交' onClick={this.changeUserInfo.bind(this)}/>
             </div>
         )
     }
