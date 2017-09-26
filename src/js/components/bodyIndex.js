a22b823cb9dd2442651ca6b0a889faaa8b1c2d44
yanlele <331393627@qq.com>
2017-09-26 16:56:41 +0800

    state

--- src/js/components/bodyIndex.js
+++ src/js/components/bodyIndex.js
@@ -1,31 +1,24 @@
 import React, {Component} from 'react'
 
 export default class BodyIndex extends Component {
-
-    componentWillMount(){
-        //将要加载
-        console.log('BodyIndex - componentWillMount')
+    constructor() {
+        super();//调用父类的所有初始化方法
+        this.state = {username: 'Parry'};
     }
-
-    componentDidMount(){
-        //加载完成
-        console.log('BodyIndex-componentDidMount')
-    }
-
 
     render() {
-        var userName='parry123123';
-        var boolInput=false;
+        setTimeout(() => {
+            //更改state的时候
+            this.setState({
+                username: 'Tom'
+            })
+        }, 4000);
 
-        var html="yanle   &nbsp;&nbsp;&nbsp;&nbsp;   testProject";
+        var html = "yanle   &nbsp;&nbsp;&nbsp;&nbsp;   testProject";
         return (
             <div>
                 <h2>页面的主体内容</h2>
-                <p>{userName==''?'用户没有登录':userName}</p>
-                <p><input type="button" value={userName} disabled={boolInput}/></p>
-                {/*注释*/}
-                <p>{html}</p>
-                <p dangerouslySetInnerHTML={{__html:html}}></p>
+                <p>{this.state.username}</p>
             </div>
         )
     }
