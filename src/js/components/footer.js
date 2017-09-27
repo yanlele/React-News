e83898abe43158b590579d7bfa09f00d35015480
yanlele <331393627@qq.com>
2017-09-27 12:25:51 +0800

    css模块化

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -1,9 +1,10 @@
 import React,{Component} from 'react'
+import '../../css/footer.css'
 
 export default class ComponentFooter extends Component{
     render(){
         return(
-            <footer>
+            <footer className='miniFooter'>
                 <h1>这里是脚信息，放置版权等东西</h1>
             </footer>
         )
