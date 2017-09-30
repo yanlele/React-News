8020e67c22321a7dc4ff8eef06f55e5f33202002
yanlele <331393627@qq.com>
2017-09-30 22:38:00 +0800

    注册登录HTML部分完成

--- src/js/components/pc/pc_header.js
+++ src/js/components/pc/pc_header.js
@@ -16,23 +16,23 @@ const SubMenu = Menu.SubMenu;
 const TabPane = Tabs.TabPane;
 const MenuItemGroup = Menu.ItemGroup;
 
-class PCHeader extends React.Component{
-    constructor(){
+class PCHeader extends React.Component {
+    constructor() {
         super();
-        this.state={
-            current:['top'],
-            modalVisible:false,//是否可见
-            action:'login',
-            hasLogined:false,//是否登录
-            userNickName:'',//用户昵称
-            userid:0
+        this.state = {
+            current: ['top'],
+            modalVisible: false,//是否可见
+            action: 'login',
+            hasLogined: false,//是否登录
+            userNickName: '',//用户昵称
+            userid: 0
         }
     }
 
-    render(){
-        let {getFieldProps}=this.props.form;
+    render() {
+        let {getFieldProps} = this.props.form;
         //根据用户的登录状态显示不同的组件
-        const userShow=this.state.hasLogined?
+        const userShow = this.state.hasLogined ?
             <Menu.Item key="logout" class="register">
                 <Button type='primary' htmlType='button'>{this.state.userNickName}</Button>
                 &nbsp;&nbsp;
@@ -48,7 +48,7 @@ class PCHeader extends React.Component{
             <Menu.Item key="register" className="register">
                 <Icon type="appstore"/>注册/登录
             </Menu.Item>;
-        return(
+        return (
             <header>
                 <Row>
                     <Col span={2}></Col>
@@ -60,33 +60,64 @@ class PCHeader extends React.Component{
                     </Col>
                     <Col span={16}>
                         {/*放置导航*/}
-                        <Menu mode="horizontal" selectedKeys={this.state.current}>
+                        <Menu
+                            mode="horizontal"
+                            selectedKeys={this.state.current}
+                            onclick={this.handleClick.bind(this)}>
+
                             <Menu.Item key="top">
-                                <Icon type="appstore" />头条
+                                <Icon type="appstore"/>头条
                             </Menu.Item>
                             <Menu.Item key="shehui">
-                                <Icon type="appstore" />社会
+                                <Icon type="appstore"/>社会
                             </Menu.Item>
                             <Menu.Item key="guonei">
-                                <Icon type="appstore" />国内
+                                <Icon type="appstore"/>国内
                             </Menu.Item>
                             <Menu.Item key="guoji">
-                                <Icon type="appstore" />国际
+                                <Icon type="appstore"/>国际
                             </Menu.Item>
                             <Menu.Item key="yule">
-                                <Icon type="appstore" />娱乐
+                                <Icon type="appstore"/>娱乐
                             </Menu.Item>
                             <Menu.Item key="tiyu">
-                                <Icon type="appstore" />体育
+                                <Icon type="appstore"/>体育
                             </Menu.Item>
                             <Menu.Item key="keji">
-                                <Icon type="appstore" />科技
+                                <Icon type="appstore"/>科技
                             </Menu.Item>
                             <Menu.Item key="shishang">
-                                <Icon type="appstore" />时尚
+                                <Icon type="appstore"/>时尚
                             </Menu.Item>
                             {userShow}
                         </Menu>
+
+                        <Modal
+                            title="用户中心"
+                            wrapClassName="vertical-center-modal"
+                            visible={this.state.modalVisible}
+                            onCancel={() => this.setModalVisible(false)}
+                            onOk={() => this.setModalVisible(false)}
+                            okText='关闭'
+                        >
+                            <Tabs type="card">
+                                <TabPane tab="注册" key="2">
+                                    <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                                        <FormItem label='账户'>
+                                            <Input placeholder="请输入您的账号" {...getFieldProps('r_userName')}/>
+                                        </FormItem>
+                                        <FormItem label='密码'>
+                                            <Input type="password" placeholder="请输入您的密码" {...getFieldProps('r_password')}/>
+                                        </FormItem>
+                                        <FormItem label='确认密码'>
+                                            <Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')}/>
+                                        </FormItem>
+                                        <Button type="primary" htmlType="submit">注册</Button>
+                                    </Form>
+                                </TabPane>
+                            </Tabs>
+                        </Modal>
+
                     </Col>
                     <Col span={2}></Col>
                 </Row>
@@ -95,4 +126,4 @@ class PCHeader extends React.Component{
     }
 }
 
-export default PCHeader=Form.create({})(PCHeader)
+export default PCHeader = Form.create({})(PCHeader)
