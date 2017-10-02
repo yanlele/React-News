3ba515bea10a178f404513ff9053854d0fedfe5c
yanlele <331393627@qq.com>
2017-10-02 21:49:41 +0800

    PC 新闻主页图片模块开发完成

---
+++ src/js/components/pc/pc_news_image_block.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import {Card} from 'antd'
+import {Router, Route, Link, browserHistory} from 'react-router'
+
+export default class PCNewsImageBlock extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            news: '',
+
+        }
+    }
+
+    componentWillMount() {
+        let myFetchOptions = {
+            method: 'GET'
+        };
+
+        //获取新闻，复制给news变量接受信息
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=" + this.props.type + "&count=" + this.props.count, myFetchOptions)
+            .then(response => response.json())
+            .then(json => this.setState({
+                news: json
+            }));
+    }
+
+    render() {
+        const styleImage={
+            display:'block',
+            width:this.props.imageWidth,
+            height:'90px',
+        };
+        const styleH3={
+            width:this.props.imageWidth,
+            whiteSpace:'nowrap',
+            overflow:'hidden',
+            textOverflow:'ellipsis'
+        };
+
+        const {news} = this.state;
+        let newsList;
+        if (news.length) {
+            newsList = news.map((newsItem, index) => (
+               <div key={index} className="imageblock">
+                   <Link to={`detail/${newsItem.uniqueKey}`} target='_blank'>
+                       <div className="custom-image">
+                           <img src={newsItem.thumbnail_pic_s} style={styleImage} alt=''/>
+                       </div>
+                       <div className="custom-card">
+                           <h3 style={styleH3}>{newsItem.title}</h3>
+                           <p>{newsItem.author_name}</p>
+                       </div>
+                   </Link>
+               </div>
+            ))
+        } else {
+            newsList = '没有加载到新闻'
+        }
+
+        return (
+            <div className="topNewsList mt20">
+                <Card title={this.props.cartTitle} border="true" style={{width:this.props.width}}>
+                    {newsList}
+                </Card>
+            </div>
+        )
+    }
+}
--- src/js/components/pc/pc_newscontainer.js
+++ src/js/components/pc/pc_newscontainer.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {Row, Col} from 'antd';
 import {Tabs, Carousel} from 'antd';
 import PCNewsBlock from './pc_news_block'
+import PCNewsImageBlock from './pc_news_image_block'
 const TabPane = Tabs.TabPane;
 
 
-
-export default class PCNewsContainer extends React.Component{
-    render(){
+export default class PCNewsContainer extends React.Component {
+    render() {
         const settings = {
             dots: true,
             infinite: true,
@@ -15,8 +15,13 @@ export default class PCNewsContainer extends React.Component{
             slidesToShow: 1,
             autoplay: true
         };
+        const style1={
+            textAlign:'center'
+        };
 
-        return(
+        return (
+
+
             <Row>
                 <Col span={2}></Col>
                 <Col span={20} className='container'>
@@ -29,6 +34,7 @@ export default class PCNewsContainer extends React.Component{
                                 <div><img src={require('../../../images/carousel_4.jpg')} alt=""/></div>
                             </Carousel>
                         </div>
+                        <PCNewsImageBlock count={6} type="guoji" width="400px" cartTitle="国际头条" imageWidth="112px"/>
                     </div>
                     <Tabs className="tabs_news">
                         <TabPane tab="新闻" key="1">
@@ -38,6 +44,10 @@ export default class PCNewsContainer extends React.Component{
                             <PCNewsBlock count={22} type="guoji" width="100%" borderd="false"/>
                         </TabPane>
                     </Tabs>
+                    <div className={style1}>
+                        <PCNewsImageBlock count={8} type="guonei" width="100%" cartTitle="国内新闻" imageWidth="132px"/>
+                        <PCNewsImageBlock count={16} type="yule" width="100%" cartTitle="娱乐新闻" imageWidth="132px"/>
+                    </div>
                 </Col>
                 <Col span={2}></Col>
             </Row>
