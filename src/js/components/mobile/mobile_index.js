ab3dfc470ce77303b3183ec5186f8fbe2266cac0
yanlele <331393627@qq.com>
2017-10-07 00:16:07 +0800

    移动端新闻轮播图开发

--- src/js/components/mobile/mobile_index.js
+++ src/js/components/mobile/mobile_index.js
@@ -3,18 +3,33 @@ import React from 'react';
 import MobileHeader from './mobile_header'
 import MobileFooter from './mobile_footer'
 
-import {Tabs} from 'antd'
+import {Tabs, Carousel} from 'antd';
 
 import MobileList from './mobile_list'
-const TabPane =Tabs.TabPane;
+const TabPane = Tabs.TabPane;
 
-export default class MobileIndex extends React.Component{
-    render(){
-        return(
+export default class MobileIndex extends React.Component {
+    render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            autoplay: true
+        };
+        return (
             <div>
                 <MobileHeader></MobileHeader>
                 <Tabs>
                     <TabPane tab="头条" key="1">
+                        <div className="carousel">
+                            <Carousel {...settings}>
+                                <div><img src={require('../../../images/carousel_1.jpg')} alt=""/></div>
+                                <div><img src={require('../../../images/carousel_2.jpg')} alt=""/></div>
+                                <div><img src={require('../../../images/carousel_3.jpg')} alt=""/></div>
+                                <div><img src={require('../../../images/carousel_4.jpg')} alt=""/></div>
+                            </Carousel>
+                        </div>
                         <MobileList count={20} type="top"/>
                     </TabPane>
                     <TabPane tab="社会" key="2">
