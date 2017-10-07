a7fbf3da659722de831762f574a30dec61e63ca2
yanlele <331393627@qq.com>
2017-10-07 09:39:27 +0800

    PC新闻新闻详情页开发

---
+++ src/js/components/pc/pc_news_details.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Row, Col, BackTop} from 'antd';
+import PCHeader from './pc_header';
+import PCFooter from './pc_footer';
+import PCNewsImageBlock from './pc_news_image_block';
+import CommonComments from './common_comments';
+export default class PCNewsDetails extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            newsItem: ''
+        };
+    };
+
+    componentDidMount() {
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions)
+            .then(response => response.json())
+            .then(json => {
+                this.setState({
+                    newsItem: json
+                });
+                document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
+            });
+    }
+
+    createMarkup() {
+        return {__html: this.state.newsItem.pagecontent};
+    }
+
+    render() {
+        return (
+            <div>
+                <Row>
+                    <Col span={2}></Col>
+                    <Col span={14} className="container">
+                        <div className="article-content" dangerouslySetInnerHTML={this.createMarkup()}></div>
+                    </Col>
+                    <Col span={6}></Col>
+                    <Col span={2}></Col>
+                </Row>
+            </div>
+        )
+    }
+}
