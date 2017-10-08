3adeb1f38cad6e9fd12071fc9bcf63dfb103383f
yanlele <331393627@qq.com>
2017-10-08 09:35:08 +0800

    pc的个人中心部分，架子大体搭建完成

---
+++ src/js/components/pc/pc_usercenter.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Row, Col, Modal} from 'antd';
+import {Menu, Icon} from 'antd';
+import PCHeader from './pc_header';
+import PCFooter from './pc_footer';
+
+import {
+    Tabs,
+    message,
+    Form,
+    Input,
+    Button,
+    Checkbox,
+    Card,
+    notification,
+    Upload
+} from 'antd';
+const FormItem = Form.Item;
+const TabPane = Tabs.TabPane;
+// import {Router, Route, Link, browserHistory} from 'react-router'
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+export default class PCUserCenter extends React.Component {
+    render() {
+        return (
+            <div>
+                <PCHeader/>
+                <Tabs>
+                    <TabPane tab="我的搜藏" key="1">
+
+                    </TabPane>
+                    <TabPane tab="我的搜评论" key="2">
+
+                    </TabPane>
+                    <TabPane tab="头像设置" key="3">
+
+                    </TabPane>
+                </Tabs>
+                <PCFooter/>
+            </div>
+        )
+    }
+}
--- src/js/router/router.js
+++ src/js/router/router.js
@@ -9,6 +9,7 @@ import PCIndex from '../components/pc/pc_index'
 import MobileIndex from '../components/mobile/mobile_index'
 import MobileNewsDetails from '../components/mobile/mobile_news_detail'
 import PCNewsDetails from '../components/pc/pc_news_details'
+import PCUserCenter from '../components/pc/pc_usercenter'
 
 
 export default class Root extends React.Component{
@@ -19,6 +20,7 @@ export default class Root extends React.Component{
                     <Router history={hashHistory}>
                         <Route path="/" component={PCIndex}/>
                         <Route path="/details/:uniquekey" component={PCNewsDetails} />
+                        <Route path="/usercenter" component={PCUserCenter}/>
                     </Router>
                 </MediaQuery>
                 <MediaQuery query="(max-device-width:1224px)">
