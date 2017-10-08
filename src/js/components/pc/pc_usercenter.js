4d5d2a3583664bbaf47f3be95ec7221897651134
yanlele <331393627@qq.com>
2017-10-08 09:55:43 +0800

    PC个人头像上传

--- src/js/components/pc/pc_usercenter.js
+++ src/js/components/pc/pc_usercenter.js
@@ -23,7 +23,37 @@ const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 export default class PCUserCenter extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            usercollection: '',
+            usercomments: '',
+            previewImage: '',
+            previewVisible: false
+        };
+    };
+
     render() {
+        const props={
+            action:'http://newsapi.gugujiankong.com/handler.ashx',
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            listType: 'picture-card',
+            defaultFileList: [
+                {
+                    uid: -1,
+                    name: 'xxx.png',
+                    state: 'done',
+                    url: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png',
+                    thumbUrl: 'https://os.alipayobjects.com/rmsportal/NDbkJhpzmLxtPhB.png'
+                }
+            ],
+            onPreview: (file) => {
+                this.setState({previewImage: file.url, previewVisible: true});
+            }
+        }
+
         return (
             <div>
                 <PCHeader/>
@@ -38,7 +68,15 @@ export default class PCUserCenter extends React.Component {
 
                             </TabPane>
                             <TabPane tab="头像设置" key="3">
-
+                                <div className="clearfix">
+                                    <Upload {...props}>
+                                        <Icon type="plus"/>
+                                        <div className="ant-upload-text">上传照片</div>
+                                    </Upload>
+                                    <Modal visible={this.state.previewVisible} footer={null} onCancel={this.handleCancel}>
+                                        <img src={this.state.previewImage} alt="预览"/>
+                                    </Modal>
+                                </div>
                             </TabPane>
                         </Tabs>
                     </Col>
